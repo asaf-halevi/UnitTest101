725caf6f192617c5f8ec63c36ca244c663bbc072
Halevi, Asaf <asaf.halevi@cellwize.com>
2022-12-27 20:22:22 +0200

    [0000] add calculator

---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Calculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int wrongAdd(int a, int b) {
+        return a + b + 1;
+    }
+}
---
+++ src/test/java/org/example/CalculatorTest.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CalculatorTest {
+
+    @Test
+    @DisplayName("Should return the sum of a and b plus 1")
+    void wrongAddShouldReturnTheSumOfAAndBPlus1() {
+        Calculator calculator = new Calculator();
+        int result = calculator.wrongAdd(1, 2);
+        assertEquals(4, result);
+    }
+
+    @Test
+    @DisplayName("Should return the sum of a and b")
+    void aWhenReturnSumOfAAndB() {
+        Calculator calculator = new Calculator();
+        int result = calculator.add(1, 2);
+        assertEquals(3, result);
+    }
+}

cc97df99ebb63987024d474b010415ee7842ed22
Halevi, Asaf <asaf.halevi@cellwize.com>
2022-12-27 15:09:34 +0200

    [0000] first commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,20 @@
 package org.example;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
+
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Main main = new Main();
+        main.useDivider();
+    }
+
+    private void useDivider() {
+        Divider divider = new Divider();
+        int result = divider.divide(4, 2); // try divide by 0
+        logger.info("Result of divider is {}", result);
     }
 }

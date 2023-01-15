f6b36fa8861a011578317f4e2372038763fabba4
Halevi, Asaf <asaf.halevi@cellwize.com>
2023-01-15 13:41:41 +0200

    [0000] refactor name of class

--- src/main/java/org/example/CoverageFooledUser.java
+++ src/main/java/org/example/CoverageFooledUser.java
@@ -8,13 +8,13 @@ public class CoverageFooledUser {
     private static final Logger logger = LoggerFactory.getLogger(CoverageFooledUser.class);
 
     public static void main(String[] args) {
-        CoverageFooledUser main = new CoverageFooledUser();
-        main.foolCoverage();
+        CoverageFooledUser coverageFooledUser = new CoverageFooledUser();
+        coverageFooledUser.foolCoverage();
     }
 
     private void foolCoverage() {
         CoverageFooled coverageFooled = new CoverageFooled();
-        String result = coverageFooled.getNameWithPrefix("Jack", 'M'); // try divide by 0
+        String result = coverageFooled.getNameWithPrefix("Jack", 'M');
         logger.info("{}", result);
     }
 }

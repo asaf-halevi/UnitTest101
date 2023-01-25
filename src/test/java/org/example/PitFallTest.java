177f81ec8e415d5d50e56f34e5c4876f98d5f049
Halevi, Asaf <asaf.halevi@cellwize.com>
2023-01-25 14:15:13 +0200

    [0000] fix PitFallTest.java

--- src/test/java/org/example/PitFallTest.java
+++ src/test/java/org/example/PitFallTest.java
@@ -1,7 +1,5 @@
 package org.example;
 
-import jdk.nashorn.internal.ir.annotations.Ignore;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -14,17 +12,17 @@ class PitFallTest {
     void divideTestDivideByNumber() {
         final int actual = pitFall.divide(1, 1);
 
-        assertEquals(2, actual);
+        assertEquals(1, actual);
     }
 
-    @Test
-    @Ignore
-    void divideTestDivideByZero() {
-        ArithmeticException thrown = Assertions.assertThrows
-                                                       (ArithmeticException.class, () -> {
-                                                           pitFall.divide(4, 0);
-                                                       });
-
-        Assertions.assertEquals("/ by zero", thrown.getMessage());
-    }
+//    @Test
+//    @Ignore
+//    void divideTestDivideByZero() {
+//        ArithmeticException thrown = Assertions.assertThrows
+//                                                       (ArithmeticException.class, () -> {
+//                                                           pitFall.divide(4, 0);
+//                                                       });
+//
+//        Assertions.assertEquals("/ by zero", thrown.getMessage());
+//    }
 }
